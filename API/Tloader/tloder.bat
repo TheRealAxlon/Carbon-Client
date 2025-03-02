6c52e04b46e3f7701764ea3f252b2ae556602db6
TheRealAxlon <axlonumni@gmail.com>
2025-03-02 11:30:24 +0000

    Update tloder.bat

--- API/Tloader/tloder.bat
+++ API/Tloader/tloder.bat
@@ -4,29 +4,34 @@ echo ============================
 echo          Tools
 echo ============================
 
-:: Ensure the Api folder exists
-set "api_folder=%USERPROFILE%\Documents\CARBON\Source\Api"
-if not exist "%api_folder%" mkdir "%api_folder%"
-
-:: Download and prepare the tools loader (tloder.bat) in the Api folder
-echo Downloading tools list...
-set "tools_list_link=https://raw.githubusercontent.com/TheRealAxlon/Carbon-Client/refs/heads/main/Apps/Tools/List.txt"
-curl -s "%tools_list_link%" -o "%api_folder%\tools_list.txt"
-
-:: Check if the tools list is downloaded
-if not exist "%api_folder%\tools_list.txt" (
-    echo Failed to fetch tools. Returning to Main Menu.
-    pause
-    goto start
+:: List available tools
+setlocal enabledelayedexpansion
+set /a count=0
+for %%A in ("%tools_folder%\*.bat") do (
+    set /a count+=1
+    echo [!count!] %%~nA
+    set "tool!count!=%%A"
 )
 
-:: Create tloder.bat in the Api folder to handle tools
-echo echo Running Tools Loader... > "%api_folder%\tloder.bat"
-echo for /f "tokens=1,* delims=;" %%A in ('type "%api_folder%\tools_list.txt"') do ( >> "%api_folder%\tloder.bat"
-echo     start "" "%%B" >> "%api_folder%\tloder.bat"
-echo ) >> "%api_folder%\tloder.bat"
+:: Add option to return to the main menu
+set /a count+=1
+echo [!count!] Back to Main Menu
 
-:: Run the tools loader (tloder.bat)
-start "" "%api_folder%\tloder.bat"
+:: Prompt user for choice
+set /p tool_choice="Select an option: "
+if "!tool_choice!"=="%count%" goto start
+
+:: Run the selected tool
+for /L %%I in (1,1,%count%-1) do (
+    if "!tool_choice!"=="%%I" (
+        echo Running tool: !tool%%I!
+        start "" "!tool%%I!"  :: Use "start" to open the tool in a new process
+        pause
+        goto Tools
+    )
+)
+
+:: Invalid option handling
+echo Invalid option. Please try again.
 pause
-goto start
+goto Tools
