69623bb952992e8bf6ead49f715e4ce514afe53e
TheRealAxlon <axlonumni@gmail.com>
2025-03-02 11:30:01 +0000

    Update oloder.bat

--- API/Tloader/oloder.bat
+++ API/Tloader/oloder.bat
@@ -4,29 +4,34 @@ echo ============================
 echo          Other
 echo ============================
 
-:: Ensure the Api folder exists
-set "api_folder=%USERPROFILE%\Documents\CARBON\Source\Api"
-if not exist "%api_folder%" mkdir "%api_folder%"
-
-:: Download and prepare the other loader (oloder.bat) in the Api folder
-echo Downloading other list...
-set "other_list_link=https://raw.githubusercontent.com/TheRealAxlon/Carbon-Client/refs/heads/main/Apps/Otherr/List.txt"
-curl -s "%other_list_link%" -o "%api_folder%\other_list.txt"
-
-:: Check if the other list is downloaded
-if not exist "%api_folder%\other_list.txt" (
-    echo Failed to fetch other apps. Returning to Main Menu.
-    pause
-    goto start
+:: List available items in "Other"
+setlocal enabledelayedexpansion
+set /a count=0
+for %%A in ("%other_folder%\*.bat") do (
+    set /a count+=1
+    echo [!count!] %%~nA
+    set "tool!count!=%%A"
 )
 
-:: Create oloder.bat in the Api folder to handle other apps
-echo echo Running Other Apps Loader... > "%api_folder%\oloder.bat"
-echo for /f "tokens=1,* delims=;" %%A in ('type "%api_folder%\other_list.txt"') do ( >> "%api_folder%\oloder.bat"
-echo     start "" "%%B" >> "%api_folder%\oloder.bat"
-echo ) >> "%api_folder%\oloder.bat"
+:: Add option to return to the main menu
+set /a count+=1
+echo [!count!] Back to Main Menu
 
-:: Run the other loader (oloder.bat)
-start "" "%api_folder%\oloder.bat"
+:: Prompt user for choice
+set /p tool_choice="Select an option: "
+if "!tool_choice!"=="%count%" goto start
+
+:: Run the selected item
+for /L %%I in (1,1,%count%-1) do (
+    if "!tool_choice!"=="%%I" (
+        echo Running item: !tool%%I!
+        start "" "!tool%%I!"  :: Use "start" to open the item in a new process
+        pause
+        goto Other
+    )
+)
+
+:: Invalid option handling
+echo Invalid option. Please try again.
 pause
-goto start
+goto Other
