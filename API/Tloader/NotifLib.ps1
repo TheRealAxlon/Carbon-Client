37fa51c5f5fa06384ce6beafb996fb81ae3b2d9e
TheRealAxlon <axlonumni@gmail.com>
2025-03-08 11:39:07 +0000

    Update NotifLib.ps1

--- API/Tloader/NotifLib.ps1
+++ API/Tloader/NotifLib.ps1
@@ -1,1 +1,7 @@
-404: Not Found
+param(
+    [string]$Title,
+    [string]$Message
+)
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.ToastNotification]::new() | Out-Null  # Dummy line; not used for MessageBox
+[System.Windows.Forms.MessageBox]::Show($Message, $Title)
